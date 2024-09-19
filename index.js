18c242878162e433bb8dd48fc01b43c6cd2a99e0
Pearl Phahlane <pearlulumotaung@gmail.com>
2024-09-19 16:41:19 +0200

    create instances of the BankBranch class with branch information

--- index.js
+++ index.js
@@ -12,3 +12,6 @@ class BankBranch {
     return this.branchInfo;
   }
 }
+
+const branchA = new BankBranch ({ name: "BNF Branch", address: "27, Melrose Arch, The High St, Sandton, Johannesburg, 2196" });
+const branchB = new BankBranch ({ name: "ASBA", address: "21 Cradock Ave, Rosebank, Johannesburg, 2196" });
